80cbb991be518d0d0b1f2e3086a15351a4daa30b
Brian King <free.as.in.speech@gmail.com>
2015-05-13 15:57:45 +0200

    add recording label; toggle hiding label

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+        //TODO: record audio
+        println("in recordAudio")
+        recordingLabel.hidden = !recordingLabel.hidden
+    }
 
 }
 
