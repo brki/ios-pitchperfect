b34d7ed0d8fbdb84279949b3bd8742fd5923e351
Brian King <free.as.in.speech@gmail.com>
2015-05-24 21:41:14 +0200

    add "chipmunk" and "darth vader" effects

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 import Foundation
 import AVFoundation
 
-class PlaySoundsViewController: UIViewController {
+class PlaySoundsViewController: UIViewController, AVAudioPlayerDelegate {
 
     @IBOutlet weak var stopPlayButton: UIButton!
     
+    var audioEngine: AVAudioEngine?
+    var audioFile: AVAudioFile?
     var audioPlayer: AVAudioPlayer?
     var receivedAudio: RecordedAudio!
     
@@ -25,9 +27,22 @@ class PlaySoundsViewController: UIViewController {
         if let error = createError {
             println("Error: \(error.localizedDescription)")
         } else {
+            audioPlayer!.delegate = self
             audioPlayer!.enableRate = true
             // Pre-load the audio file to minimize delay when play() is called:
             audioPlayer!.prepareToPlay()
+        }
+        
+        // Initialize the audioEngine and the file it will use:
+        audioEngine = AVAudioEngine()
+        if let engine = audioEngine {
+            var fileError: NSError?
+            audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: &fileError)
+            if let error = fileError {
+                println("Error intializing AVAudioFile: \(error.localizedDescription)")
+            }
+        } else {
+            println("Error intializing AVAudioEngine")
         }
     }
     
@@ -43,11 +58,49 @@ class PlaySoundsViewController: UIViewController {
     @IBAction func playSlowSound(sender: UIButton) {
         playSoundAtRate(0.5)
     }
-
+    
     @IBAction func playFastSound(sender: UIButton) {
         playSoundAtRate(2.0)
     }
 
+    @IBAction func playChipmunkAudio(sender: UIButton) {
+        playAudioAtPitch(900)
+    }
+
+    @IBAction func playDarthVaderAudio(sender: UIButton) {
+        playAudioAtPitch(-1000)
+    }
+    
+    func playAudioAtPitch(pitch: Float) {
+        if let engine = audioEngine {
+            engine.stop()
+            engine.reset()
+            let playerNode: AVAudioPlayerNode? = AVAudioPlayerNode()
+            let pitchNode: AVAudioUnitTimePitch? = AVAudioUnitTimePitch()
+            if let player = playerNode, timePitch = pitchNode {
+                timePitch.pitch = pitch
+                engine.attachNode(playerNode)
+                engine.attachNode(pitchNode)
+                engine.connect(playerNode, to: timePitch, format: nil)
+                engine.connect(pitchNode, to:engine.outputNode, format: nil)
+                
+                player.scheduleFile(audioFile, atTime: nil, completionHandler: {
+                    self.stopAudio(nil)
+                })
+                
+                var engineError: NSError?
+                engine.startAndReturnError(&engineError)
+                if let error = engineError {
+                    println("Error starting AVAudioEngine: \(error.localizedDescription)")
+                    return
+                }
+                player.play()
+                stopPlayButton.hidden = false
+            }            
+        }
+    }
+
+    
     /// Play sound at provided rate (between 0.5 and 2.0)
     /// and shows the stopPlayButton.
     func playSoundAtRate(rate: Double) {
@@ -63,11 +116,20 @@ class PlaySoundsViewController: UIViewController {
     }
     
     /// Stops the audio from playing and resets the player's currentTime to 0
-    @IBAction func stopAudio(sender: UIButton) {
+    @IBAction func stopAudio(sender: UIButton?) {
         if let player = audioPlayer {
             player.stop()
             player.currentTime = 0
         }
+        if let engine = audioEngine {
+            engine.stop()
+            engine.reset()
+        }
+        stopPlayButton.hidden = true
+    }
+    
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer!, successfully flag: Bool) {
+        stopPlayButton.hidden = true
     }
     
     /*
